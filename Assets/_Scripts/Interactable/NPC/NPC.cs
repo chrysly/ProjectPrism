fa7878fa96f97c214e11716f21da7d7506605f2c
Ivu <ivucreates@gmail.com>
2024-06-28 19:54:41 -0400

    ui changes
    save before i refactor some player things

--- Assets/_Scripts/Interactable/NPC/NPC.cs
+++ Assets/_Scripts/Interactable/NPC/NPC.cs
@@ -10,7 +10,7 @@ using UnityEngine.UI;
 public class NPC : Interactable
 {
     [SerializeField] protected Image _ImageHolder;
-    [SerializeField] protected NPCData _data;
+    [SerializeField] protected NPCData _npcData;
 
     protected bool _dialogueStarted = false;
     protected int _dialogueIndex = 0;
@@ -21,19 +21,24 @@ public class NPC : Interactable
     }
 
     protected void DialogueStart() {
+        _dialogueIndex = 0;
         _dialogueStarted = true;
         _ImageHolder.gameObject.SetActive(true);
         GameManager.Instance.EnterUIControls();
         GameManager.Instance.PlayerActionMap.UIControl.Continue.performed += ContinueDialogue;
+        _ImageHolder.sprite = _npcData.ImageSeq[_dialogueIndex];
     }
 
     protected void ContinueDialogue(InputAction.CallbackContext context) {
-        if (_dialogueIndex == _data.ImageSeq.Count) {  EndDialoge(); }
+        _dialogueIndex++;
 
+        if (_dialogueIndex == _npcData.ImageSeq.Count) {  EndDialoge(); return;  }
 
+        _ImageHolder.sprite = _npcData.ImageSeq[_dialogueIndex];
     }
 
     protected void EndDialoge() {
-
+        _ImageHolder.gameObject.SetActive(false);
+        GameManager.Instance.EnterPlayerControls();
     }
 }
