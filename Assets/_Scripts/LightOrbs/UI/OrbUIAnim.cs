d799982a46952ff3736bb26cb85f0a6c8ebd7f5a
Ivu <ivucreates@gmail.com>
2024-07-25 19:48:26 -0400

    UI WORKS AAAA

--- Assets/_Scripts/LightOrbs/UI/OrbUIAnim.cs
+++ Assets/_Scripts/LightOrbs/UI/OrbUIAnim.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.Rendering.UI;
 
 public class OrbUIAnim : MonoBehaviour
 {
     [SerializeField] private float _fadeTime = 1.5f;
+    [SerializeField] private float _animationDuration = 1f;
     [SerializeField] private CanvasGroup _mainCanvas;   // for the entire canvas *cries*
     [SerializeField] private CanvasGroup _sideBitches;
     [SerializeField] private RectTransform _rectTransform;
@@ -20,10 +22,9 @@ public class OrbUIAnim : MonoBehaviour
     private Image MainHolder => _bckgImgs[0];
 
     void Start() {
-        OrbHandler.OnOrbsSwapped += StartAnim;
+        //OrbHandler.OnOrbsSwapped += StartAnim;
         OrbHandler.OnOrbThrown += OnOrbThrown;
         Player.OnSpawn += OnSpawn;
-        //_canvasGroup.alpha = 0f;
     }
 
     /// <summary>
@@ -47,133 +48,75 @@ public class OrbUIAnim : MonoBehaviour
 
     // jank bc dreamhack kms
     private IEnumerator SpawnWait() {
-        yield return new WaitForSeconds(1f);
-        //FadeOutBckg();
-        for (int i = 1; i < _orbs.Count; ++i) {
-            _orbs[i].FadeOrb(0, _fadeTime);
-        }
+        yield return new WaitForSeconds(0.4f);
+        FadeBackgroundOrbs(0);
     }
 
     private void OnOrbThrown(List<EColor> cls) {
-        _orbs[0].FadeOrb(0, _fadeTime);
+        _orbs[0].SpecialFade(0, 1f);
 
-        for (int i = 1; i < _orbs.Count; ++i) {
-            _orbs[i].FadeOrb(1, _fadeTime);
-            _orbs[i].MoveOrb(_bckgImgs[i].transform.localPosition, _fadeTime);
-        }
+       FadeBackgroundOrbs(1);
 
-        //FadeOutOrb(_orbImgs[0]);
-        //FadeInBckg();
-        //FadeInOrb(_orbImgs[1]);
-        //FadeInOrb(_orbImgs[2]);
-
-        //RotateOrbImages();
-    }
-
-    private void StartAnim(List<EColor> cls) {
-        FadeInBckg();
-        SpawnOrbs(cls);
+       RotateOrbImages();
     }
 
     // Utility methods ---
 
-    private void SetInitialOrbColor(List<EColor> cls) {
-        for (int i = 0; i < cls.Count; i++) {
-            _orbImgs[i].color = cls[i].GetColor();
-        }
-    }
-
-    private void FadeInBckg() {
-        for(int i = 1; i < _bckgImgs.Length; ++i) {
-            _bckgImgs[i].transform.localPosition = new Vector3(0f, -0.5f, 0f);
-            _bckgImgs[i].rectTransform.DOAnchorPos(new Vector2(0f, 0f), _fadeTime, false).SetEase(Ease.OutQuint);
-            _bckgImgs[i].DOFade(1, _fadeTime);
+    private void FadeBackgroundOrbs(int alpha) {
+        if (alpha > 0) {
+            for (int i = 1; i < _orbs.Count; ++i) {
+                _orbs[i].FadeOrb(1, _fadeTime);
+                _orbs[i].MoveOrb(_bckgImgs[i].transform.localPosition, _fadeTime);
+            }
+        } else {
+            for (int i = 1; i < _orbs.Count; ++i) {
+                _orbs[i].FadeOrb(0, _fadeTime);
+            }
         }
         
-        //_canvasGroup.alpha = 0f;
-        //_rectTransform.transform.localPosition = new Vector3(0f, -0.5f, 0f);
-        //_rectTransform.DOAnchorPos(new Vector2(0f, 0f), _fadeTime, false).SetEase(Ease.OutQuint);
-        //_canvasGroup.DOFade(1, _fadeTime);
-    }
-    private void FadeOutBckg() {
-        for (int i = 1; i < _bckgImgs.Length; ++i) {
-            _bckgImgs[i].DOFade(0, _fadeTime);
-        }
-        //_rectTransform.transform.localPosition = new Vector3(0f, -0.5f, 0f);
-        //_rectTransform.DOAnchorPos(new Vector2(0f, 0f), _fadeTime, false).SetEase(Ease.OutQuint);
-
-        //_canvasGroup.DOFade(0, _fadeTime);
-    }
-
-    private void SpawnOrbs(List<EColor> cls) {
-        //for (int i = 0; i < _orbs.Count; i++) {
-        //    FadeInOrb(_orbImgs[i]);
-        //}
     }
 
     private void RotateOrbImages() {
-        //if (_orbImgs == null || _orbImgs.Count == 0) return;
-        //StartCoroutine(RotatePositionsCoroutine());
+        if (_orbs == null || _orbs.Count == 0) return;
+        StartCoroutine(RotatePositionsCoroutine());
     }
 
-    //// might refactor this but if it works for the mvp eh
-    //private IEnumerator RotatePositionsCoroutine() {
-    //    yield return new WaitForSeconds(1f);
+    // might refactor this but if it works for the mvp eh
+    private IEnumerator RotatePositionsCoroutine() {
+        yield return new WaitForSeconds(1f);
 
-    //    int numImg = _orbImgs.Count;
-    //    Vector3[] startPos = new Vector3[numImg];
-    //    Vector3[] endPos = new Vector3[numImg];
+        int numImg = _orbs.Count;
+        Vector2[] endPos = new Vector2[numImg];
 
-    //    // store the starting positions
-    //    for (int i = 0; i < numImg; i++) { startPos[i] = _orbImgs[i].rectTransform.position; }
+        // determine end pos
+        for (int i = 0; i < numImg; i++) {
+            int nextIndex = (i + 1) % numImg;
+            endPos[nextIndex] = _bckgImgs[i].rectTransform.localPosition;
+        }
 
-    //    // determine end pos
-    //    for (int i = 0; i < numImg; i++) {
-    //        int nextIndex = (i + 1) % numImg;
-    //        endPos[nextIndex] = startPos[i];
-    //    }
+        float elapsedTime = 0f;
 
-    //    float elapsedTime = 0f;
+        while (elapsedTime < _animationDuration) {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / _animationDuration);
 
-    //    while (elapsedTime < _animationDuration) {
-    //        elapsedTime += Time.deltaTime;
-    //        float t = Mathf.Clamp01(elapsedTime / _animationDuration);
+            // slerp positions
+            for (int i = 0; i < numImg; ++i) {
+                _orbs[i].MoveToPos(endPos[i], t);
+            }
+            yield return null;
+        }
 
-    //        // slerp positions
-    //        for (int i = 0; i < numImg; i++) {
-    //            _orbImgs[i].rectTransform.position = Vector3.Slerp(startPos[i], endPos[i], t);
-    //        }
-    //        yield return null;
-    //    }
+        // ensure final positions are set
+        //for (int i = 0; i < numImg; i++) { _orbs[i].SetUIOrbPos(endPos[i]); }
 
-    //    // ensure final positions are set
-    //    for (int i = 0; i < numImg; i++) { _orbImgs[i].rectTransform.position = endPos[i];  }
-
-    //    // rotate img data in list
-    //    //Image lastImg = _orbImgs[_orbImgs.Count - 1];
-    //    //for (int i = numImg - 1; i > 0; i--) {
-    //    //    _orbImgs[i] = _orbImgs[i - 1];
-    //    //}
-    //    //_orbImgs[0] = lastImg;
-
-    //    //Image[] tempImages = new Image[numImg];
-    //    //for (int i = 0; i < numImg; i++) {
-    //    //    tempImages[(i + 1) % numImg] = _orbImgs[i];
-    //    //}
-
-    //    //for (int i = 0; i < numImg; i++) {
-    //    //    _orbImgs[i] = tempImages[i];
-    //    //}
-
-    //    // hard coded shit
-    //    Image temp = _orbImgs[0];
-    //    _orbImgs.Remove(temp);
-    //    _orbImgs.Add(temp);
+        // hard coded shit
+        UIOrb temp = _orbs[0];
+        _orbs.Remove(temp);
+        _orbs.Add(temp);
 
 
-    //    yield return new WaitForSeconds(0.5f);
-    //    FadeOutBckg();
-    //    FadeOutOrb(_orbImgs[1]);
-    //    FadeOutOrb(_orbImgs[2]);
-    //}
+        yield return new WaitForSeconds(0.1f);
+        FadeBackgroundOrbs(0);
+    }
 }
--- Assets/_Scripts/LightOrbs/UI/UIOrb.cs
+++ Assets/_Scripts/LightOrbs/UI/UIOrb.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System;
 
 /// <summary>
 /// Class for the OrbUI Objects
 /// </summary>
 public class UIOrb
 {
-    private Transform _currPos;
+    private Transform _currPos; //???
     private Image _img;
 
     public UIOrb(Image i, Transform t) {
@@ -32,11 +33,20 @@ public class UIOrb
         else {
             _img.rectTransform.DOAnchorPos(new Vector2(0f, 0f), fadeTime, false).SetEase(Ease.OutQuint);
         }
+        SpecialFade(alpha, fadeTime);
+    }
+
+    // please refactor future self. this is dumb asf ;__;
+    public void SpecialFade(int alpha, float fadeTime) {
         _img.DOFade(alpha, fadeTime);
     }
 
     public void MoveOrb(Vector2 pos, float time) {
         _img.rectTransform.DOAnchorPos(pos, time, false).SetEase(Ease.OutQuint);
+    }
+
+    public void MoveToPos(Vector2 endPos, float time) {
+        _img.rectTransform.localPosition = Vector3.Slerp(_img.rectTransform.localPosition, endPos, 0.01f);
     }
 
     public void SetOrbColor(EColor color) { _img.color = color.GetColor(); }
