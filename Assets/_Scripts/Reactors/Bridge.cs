976f7709645c16db4b43afb584b2f420ef82debd
Carlijavier5 <carlijavier5@gmail.com>
2024-07-29 02:15:42 -0400

    Alter Anims+ Pushable Patch

--- Assets/_Scripts/Reactors/Bridge.cs
+++ Assets/_Scripts/Reactors/Bridge.cs
@@ -11,13 +11,19 @@ public class Bridge : Togglable {
     private void Awake() {
         _renderer = GetComponent<MeshRenderer>();
         _collider = GetComponentInChildren<BoxCollider>();
+
+        MaterialPropertyBlock mpb = new();
+        _renderer.GetPropertyBlock(mpb);
+        mpb.SetFloat("_Cutoff", -0.6f);
+        _renderer.SetPropertyBlock(mpb);
     }
 
     protected override void Enable(OrbThrownData data) {
         base.Enable(data);
         Material material = new Material(_renderer.materials[0]);
-        material.SetColor("_Color", data.OrbObject.GetComponentInChildren<MeshRenderer>().materials[0].GetColor("_Color"));
-        material.DOFloat(1.25f, "_Cutoff", 3f);
+        material.SetColor("_Color", data.Color.GetColor());
+        //material.DOFloat(1.25f, "_Cutoff", 3f);
+        StartCoroutine(UpdateCutoff(0.35f, 1.5f));
         Material[] materials = { material };
         _renderer.materials = materials;
         _collider.enabled = true;   //TODO: refactor to dynamic collider toggle, should move with the shader
@@ -25,10 +31,24 @@ public class Bridge : Togglable {
 
     protected override void Disable(OrbThrownData data) {
         Material material = new Material(_renderer.materials[0]);
-        material.SetColor("_Color", data.OrbObject.GetComponentInChildren<MeshRenderer>().materials[0].GetColor("_Color"));
-        material.DOFloat(0f, "_Cutoff", 1.5f);
+        material.SetColor("_Color", data.Color.GetColor());
+        //material.DOFloat(0f, "_Cutoff", 1.5f);
+        StartCoroutine(UpdateCutoff(-0.6f, 0.75f));
         Material[] materials = { material };
         _renderer.materials = materials;
         _collider.enabled = false;
+    }
+
+    private IEnumerator UpdateCutoff(float target, float time) {
+        MaterialPropertyBlock mpb = new();
+        _renderer.GetPropertyBlock(mpb);
+        float initialVal = mpb.GetFloat("_Cutoff");
+        float lerpValue = 0;
+        while (lerpValue < 1) {
+            lerpValue = Mathf.MoveTowards(lerpValue, 1, Time.deltaTime * time);
+            mpb.SetFloat("_Cutoff", Mathf.Lerp(initialVal, target, lerpValue));
+            _renderer.SetPropertyBlock(mpb);
+            yield return null;
+        }
     }
 }
