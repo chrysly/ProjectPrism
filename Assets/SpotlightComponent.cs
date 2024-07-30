cf6959a4d942b37cbef526bedab0d70f2f230d42
chrysly <chrysly0816@gmail.com>
2024-07-29 21:54:11 -0400

    Spotlight

---
+++ Assets/SpotlightComponent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightComponent : MonoBehaviour {
+    [SerializeField] private Renderer spotlight;
+    [SerializeField] private Renderer ringLight;
+    
+}
